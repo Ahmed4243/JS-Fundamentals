a7bf29b0919f31be47d60ecb3651df6185907ec5
Ahmed4243 <ahmedmohammed16.am@gmail.com>
2025-05-28 12:35:53 +0300

    2-arguments.js differentiates between 1 and 2 arguments now

--- 2-arguments.js
+++ 2-arguments.js
@@ -3,6 +3,8 @@ const userArgs = argv.slice(2);
 
 if (userArgs.length == 0){
     console.log("No argument found")
-} else{
+} else if (userArgs.length == 1){
+    console.log("Argument found")
+ }else {
     console.log("Arguments found")
-}
+ }
