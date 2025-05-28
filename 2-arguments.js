14d8b995f3419f28cdc9705e4bcb1ba6e7e2266d
Ahmed4243 <ahmedmohammed16.am@gmail.com>
2025-05-28 12:36:50 +0300

    Small fixes

--- 2-arguments.js
+++ 2-arguments.js
@@ -2,7 +2,7 @@ const { argv } = require('node:process'); // Destructuring assignment
 const userArgs = argv.slice(2);
 
 if (userArgs.length == 0){
-    console.log("No argument found")
+    console.log("No argument")
 } else if (userArgs.length == 1){
     console.log("Argument found")
  }else {
