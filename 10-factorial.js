fc83c75d5022b5645b2ea18958e58535b0199d70
Ahmed4243 <ahmedmohammed16.am@gmail.com>
2025-05-28 16:26:09 +0300

    10-factorial.js

---
+++ 10-factorial.js
@@ -0,0 +1,15 @@
+const { argv } = require("node:process");
+const userArgs = argv.slice(2);
+
+const a = Number(userArgs[0]);
+
+if (!isNaN(a)) {
+    console.log(fact(a));
+}
+
+function fact(n) {
+    if (n === 0||n === 1) {
+        return 1;
+    }
+    return  n * fact(n - 1);
+}
